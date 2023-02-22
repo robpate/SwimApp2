3381eef236eec231e554d79fb4b0e70eb09fea6d
Robert Paterson <robpate@chbc.school.nz>
2023-02-22 14:10:36 +1300

    OneSwimmer() -record swimmer details -calculate swimming average -assign swimmer team

--- SwimApp2/Program.cs
+++ SwimApp2/Program.cs
@@ -8,8 +8,102 @@ namespace SwimApp2
 {
     internal class Program
     {
+
+        //Global variables
+        static List<string> teamA = new List<string>();
+        static List<string> teamB = new List<string>();
+        static List<string> teamReserve = new List<string>();
+
+        static float fastestTime = 9999f;
+        static string topSwimmer = "";
+        static void OneSwimmer()
+        {
+            Console.WriteLine("Enter the swimmer's name");
+
+            string swimmerName = Console.ReadLine();
+
+            Console.WriteLine($"Swimmer Name: {swimmerName}");
+
+            int sumTotalTime = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                int minutes, seconds, totalTime;
+
+                Random randomNumber = new Random();
+
+                // Generate random int between 1, 4 incl
+                minutes = randomNumber.Next(1, 4);
+
+                // Generate random int between 0, 59 incl
+                seconds = randomNumber.Next(0, 59);
+
+                totalTime = (minutes * 60) + seconds;
+
+                Console.WriteLine($"Swimmer time {i+1}: {minutes}min {seconds}s\tTotal time in seconds: {totalTime}s");
+
+                sumTotalTime += totalTime;
+
+                Console.WriteLine(sumTotalTime);
+
+
+
+                Console.ReadLine();
+            }
+
+            float avgTime = (float)sumTotalTime / 4;
+
+            if (avgTime > fastestTime)
+            {
+                fastestTime = avgTime;
+                topSwimmer = swimmerName;
+
+            }
+
+            string allocatedTeam = "Reserve";
+
+            Console.WriteLine($"Avg time: {avgTime}s");
+
+            //Assign the swimmer to a team
+
+            if (avgTime <= 160)
+            {
+                teamA.Add(swimmerName);
+                allocatedTeam = "A";
+            }
+            else if (avgTime <= 210)
+            {
+                allocatedTeam = "B";
+                teamB.Add(swimmerName);
+            }
+            else
+            {
+                teamReserve.Add(swimmerName);
+                allocatedTeam = "Reserve D:";
+            }
+            
+            
+            Console.WriteLine(allocatedTeam);
+           
+           
+            
+        }
+
         static void Main(string[] args)
         {
+            
+            string flag = "";
+            while(!flag.Equals("stop"))
+            {
+                OneSwimmer();
+
+                Console.WriteLine("Press <Enter> to add another swimmer or type 'stop' to end ");
+
+                flag = Console.ReadLine();
+            }
+
+            Console.WriteLine($"the fastest swimmer was {topSwimmer} with an average time of {fastestTime}");
+            Console.ReadLine();
         }
     }
 }
